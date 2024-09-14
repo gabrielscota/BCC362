4fe1598dcc62bd7deffc99e185fa3b8e9cb88782
Gabriel Scotá <gabrielscota2015@gmail.com>
2024-09-14 15:47:41 -0300

    🛠️: update analysis options and configure sync services in Docker Compose

--- bin/sync_server.dart
+++ bin/sync_server.dart
@@ -1,9 +1,9 @@
+import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
-import 'package:googleapis/pubsub/v1.dart' as pubsub;
 import 'package:googleapis/pubsub/v1.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:logging/logging.dart';
@@ -14,43 +14,51 @@ import 'package:shelf/shelf_io.dart' as io;
 final Queue<Map<String, dynamic>> messageQueue = Queue();
 
 void setupLogging() {
-  // Configura o formato dos logs
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((record) {
     print('[${record.loggerName}] [${record.level.name}]: ${record.message}');
   });
 }
 
-Future<pubsub.PubsubApi> createPubSubClient() async {
-  var accountCredentials = ServiceAccountCredentials.fromJson(File('service-account.json').readAsStringSync());
+Future<PubsubApi> createPubSubClient() async {
+  final ServiceAccountCredentials accountCredentials =
+      ServiceAccountCredentials.fromJson(File('service-account.json').readAsStringSync());
 
-  var scopes = [pubsub.PubsubApi.cloudPlatformScope];
+  final List<String> scopes = [PubsubApi.cloudPlatformScope];
 
-  var authClient = await clientViaServiceAccount(accountCredentials, scopes);
-  return pubsub.PubsubApi(authClient);
+  final AutoRefreshingAuthClient authClient = await clientViaServiceAccount(accountCredentials, scopes);
+
+  return PubsubApi(authClient);
 }
 
+enum MessagePrimitive { ACQUIRE, RELEASE }
+
 class SyncServer {
-  final String serverId;
+  final String clusterSyncId;
   final String topic;
   final String subscription;
-  final pubsub.PubsubApi pubSubClient;
+  final PubsubApi pubSubClient;
   final Logger logger;
 
   SyncServer({
-    required this.serverId,
+    required this.clusterSyncId,
     required this.topic,
     required this.subscription,
     required this.pubSubClient,
     required this.logger,
   });
 
-  // Envia uma mensagem de ACQUIRE ao Pub/Sub com timestamp
-  Future<void> publishAcquireMessage(int acquireTimestamp) async {
+  List<Map<int, Completer<bool>>> completerList = [];
+
+  // Publica uma mensagem de ACQUIRE ao Pub/Sub com timestamp
+  Future<void> publishAcquireMessage(Completer<bool> completer) async {
+    final int messageId = DateTime.now().millisecondsSinceEpoch;
+    completerList.add({messageId: completer});
+
     final message = json.encode({
-      'serverId': serverId,
-      'action': 'ACQUIRE',
-      'timestamp': acquireTimestamp, // Mantém o timestamp de ACQUIRE
+      'clusterSyncId': clusterSyncId,
+      'primitive': MessagePrimitive.ACQUIRE.name,
+      'messageId': messageId,
     });
     await pubSubClient.projects.topics.publish(
       PublishRequest(
@@ -62,11 +70,11 @@ class SyncServer {
   }
 
   // Envia uma mensagem de RELEASE ao Pub/Sub, incluindo o timestamp original de ACQUIRE
-  Future<void> publishReleaseMessage(int acquireTimestamp) async {
+  Future<void> publishReleaseMessage(int messageId) async {
     var message = json.encode({
-      'serverId': serverId,
-      'action': 'RELEASE',
-      'timestamp': acquireTimestamp, // Usa o timestamp de ACQUIRE
+      'clusterSyncId': clusterSyncId,
+      'primitive': MessagePrimitive.RELEASE.name,
+      'messageId': messageId,
     });
     await pubSubClient.projects.topics.publish(
       PublishRequest(
@@ -78,158 +86,128 @@ class SyncServer {
   }
 
   // Escuta mensagens do Pub/Sub e processa as de ACQUIRE e RELEASE
-  Future<bool> listenForMessages(int acquireTimestamp) async {
+  Future<void> listenMessages() async {
     while (true) {
-      var response = await pubSubClient.projects.subscriptions.pull(
+      final PullResponse response = await pubSubClient.projects.subscriptions.pull(
         PullRequest(returnImmediately: false, maxMessages: 10),
         subscription,
       );
-      for (var receivedMessage in response.receivedMessages!) {
-        var message = utf8.decode(base64Decode(receivedMessage.message!.data!));
+
+      if (response.receivedMessages == null) {
+        continue;
+      }
+
+      for (final receivedMessage in response.receivedMessages!) {
+        final String message = utf8.decode(base64Decode(receivedMessage.message!.data!));
 
         try {
-          var decodedMessage = json.decode(message);
+          final decodedMessage = json.decode(message);
 
-          if (decodedMessage['action'] == 'ACQUIRE') {
-            // Verifica se o servidor pode acessar a seção crítica
-            if (_canAccessCriticalSection(decodedMessage)) {
-              // Acessa o recurso
-              await accessResource();
+          if (decodedMessage['primitive'] == MessagePrimitive.ACQUIRE.name) {
+            logger.info('Received ACQUIRE message: $decodedMessage');
 
-              // Após acessar o recurso, envia uma mensagem de RELEASE
-              await publishReleaseMessage(acquireTimestamp);
+            // Adiciona a mensagem à fila de mensagens ACQUIRE)
+            final alreadyInQueue = messageQueue.any((element) =>
+                element['clusterSyncId'] == decodedMessage['clusterSyncId'] &&
+                element['messageId'] == decodedMessage['messageId']);
+            if (!alreadyInQueue) {
+              messageQueue.add(decodedMessage);
+              logger.info('Added message (${decodedMessage['messageId']}) to queue: $messageQueue');
 
-              // Espera até que o RELEASE seja recebido
-              await waitForRelease(acquireTimestamp);
-
-              return true; // Retorna sucesso após o RELEASE
+              await pubSubClient.projects.subscriptions.acknowledge(
+                AcknowledgeRequest(ackIds: [receivedMessage.ackId!]),
+                subscription,
+              );
             }
-          } else if (decodedMessage['action'] == 'RELEASE') {}
-        } catch (e) {
-          logger.severe('Error decoding message: $e');
-        }
+          } else if (decodedMessage['primitive'] == MessagePrimitive.RELEASE.name) {
+            logger.info('Received RELEASE message: $decodedMessage');
 
-        // Confirma que a mensagem foi processada
-        await pubSubClient.projects.subscriptions.acknowledge(
-          AcknowledgeRequest(ackIds: [receivedMessage.ackId!]),
-          subscription,
-        );
-      }
-    }
-  }
+            // Remove a mensagem da fila de mensagens ACQUIRE
+            messageQueue.removeWhere((element) =>
+                element['clusterSyncId'] == decodedMessage['clusterSyncId'] &&
+                element['messageId'] == decodedMessage['messageId']);
+            logger.info('Removed message (${decodedMessage['messageId']}) from queue: $messageQueue');
 
-  // Verifica se o servidor pode acessar a seção crítica com base nas mensagens do Pub/Sub
-  bool _canAccessCriticalSection(Map<String, dynamic> decodedMessage) {
-    var serverId = decodedMessage['serverId'];
-    var action = decodedMessage['action'];
-    var messageTimestamp = decodedMessage['timestamp'];
+            await pubSubClient.projects.subscriptions.acknowledge(
+              AcknowledgeRequest(ackIds: [receivedMessage.ackId!]),
+              subscription,
+            );
 
-    if (action == 'ACQUIRE' && serverId == this.serverId) {
-      logger.info('Received ACQUIRE message from server $serverId with timestamp $messageTimestamp');
+            // Completa o Future do ACQUIRE correspondente
+            final messageId = decodedMessage['messageId'];
+            final completer = completerList.firstWhere((element) => element.containsKey(messageId)).values.first;
+            completer.complete(true);
+            completerList.removeWhere((element) => element.containsKey(messageId));
 
-      // Adiciona a mensagem à fila de mensagens ACQUIRE
-      messageQueue.add(decodedMessage);
-      logger.info(
-          'Added message ($messageTimestamp) from server $serverId to the queue. Queue size: ${messageQueue.length}');
-
-      // Verifica se o primeiro item na fila é o servidor atual
-      if (messageQueue.isNotEmpty && messageQueue.first['timestamp'] == messageTimestamp) {
-        logger.info('Server $serverId has access to the critical section');
-        return true; // O servidor pode acessar a seção crítica
-      }
-    } else if (action == 'RELEASE' && serverId == this.serverId) {
-      logger.info('Received RELEASE message ($messageTimestamp) from server $serverId');
-
-      // Remove a mensagem correspondente da fila comparando id e timestamp de ACQUIRE
-      messageQueue.removeWhere((msg) =>
-          msg['serverId'] == serverId &&
-          msg['timestamp'] == messageTimestamp); // Comparar timestamp da mensagem de ACQUIRE
-
-      logger.info(
-          'Removed message from server $serverId with timestamp $messageTimestamp from the queue. Queue size: ${messageQueue.length}');
-      return true;
-    }
-
-    return false;
-  }
-
-  // Simula o acesso ao recurso
-  Future<void> accessResource() async {
-    logger.info('Server $serverId accessing the critical section...');
-    int criticalRegionTime = 200 + Random().nextInt(801); // Tempo de acesso aleatório entre 200 e 1000ms
-    await Future.delayed(Duration(milliseconds: criticalRegionTime)); // Reduzi o tempo para milissegundos
-    logger.info('Server $serverId left the critical section.');
-  }
-
-  // Escuta mensagens do Pub/Sub e espera até a mensagem de RELEASE
-  Future<void> waitForRelease(int acquireTimestamp) async {
-    while (true) {
-      var response = await pubSubClient.projects.subscriptions.pull(
-        PullRequest(returnImmediately: false, maxMessages: 10),
-        subscription,
-      );
-      for (var receivedMessage in response.receivedMessages!) {
-        var message = utf8.decode(base64Decode(receivedMessage.message!.data!));
-
-        try {
-          var decodedMessage = json.decode(message);
-
-          // Se a mensagem de RELEASE for recebida, remove da fila e retorna
-          if (decodedMessage['action'] == 'RELEASE' &&
-              decodedMessage['timestamp'] == acquireTimestamp &&
-              decodedMessage['serverId'] == serverId) {
-            logger.info('Received RELEASE message from server $serverId with timestamp $acquireTimestamp');
-
-            // Remove a mensagem correspondente da fila
-            messageQueue.removeWhere((msg) => msg['serverId'] == serverId && msg['timestamp'] == acquireTimestamp);
-            logger.info('Message ($acquireTimestamp) removed from queue for server $serverId');
-
-            return; // Retorna quando a mensagem de RELEASE é processada
+            lastProcessedMessageId = -1;
           }
         } catch (e) {
           logger.severe('Error decoding message: $e');
         }
-
-        // Confirma que a mensagem foi processada
-        await pubSubClient.projects.subscriptions.acknowledge(
-          AcknowledgeRequest(ackIds: [receivedMessage.ackId!]),
-          subscription,
-        );
       }
     }
+  }
+
+  int lastProcessedMessageId = -1;
+
+  Future<void> processMessages() async {
+    while (true) {
+      await Future.delayed(Duration(milliseconds: 100));
+
+      if (messageQueue.isEmpty) {
+        await Future.delayed(Duration(seconds: 5));
+      } else {
+        final message = messageQueue.first;
+        if (message['clusterSyncId'] == clusterSyncId && message['messageId'] != lastProcessedMessageId) {
+          // Acessa o recurso crítico
+          await accessResource();
+
+          // Publica uma mensagem de RELEASE
+          await publishReleaseMessage(message['messageId']);
+
+          lastProcessedMessageId = message['messageId'];
+        }
+      }
+    }
+  }
+
+  // Simula o acesso ao recurso
+  Future<void> accessResource() async {
+    logger.info('Sync $clusterSyncId accessing the critical section...');
+    int criticalRegionTime = 200 + Random().nextInt(801); // Tempo de acesso aleatório entre 200 e 1000ms
+    await Future.delayed(Duration(milliseconds: criticalRegionTime));
+    logger.info('Sync $clusterSyncId left the critical section.');
   }
 }
 
 void main(List<String> args) async {
-  var serverId = args.isNotEmpty ? args[0] : '0';
+  final String clusterSyncId = args.isNotEmpty ? args[0] : '0';
 
   // Configuração do Logger
-  final Logger logger = Logger('Server $serverId');
+  final Logger logger = Logger('Sync $clusterSyncId');
   setupLogging();
 
   var pubSubClient = await createPubSubClient();
   var syncServer = SyncServer(
-    serverId: serverId,
+    clusterSyncId: clusterSyncId,
     pubSubClient: pubSubClient,
     topic: 'projects/tp2-bcc362/topics/sync-topic',
-    subscription: 'projects/tp2-bcc362/subscriptions/sync-topic-sub',
+    subscription: 'projects/tp2-bcc362/subscriptions/sync-topic-sub-$clusterSyncId',
     logger: logger,
   );
 
   // Define o handler do servidor Shelf para lidar com requisições
   var handler = const Pipeline().addMiddleware(logRequests()).addHandler((Request request) async {
     if (request.method == 'POST' && request.url.path == 'acquire') {
-      // Envia a mensagem de ACQUIRE e aguarda até poder acessar o recurso
-      final int acquireTimestamp = DateTime.now().millisecondsSinceEpoch;
-
-      await syncServer.publishAcquireMessage(acquireTimestamp);
+      final Completer<bool> completer = Completer();
+      await syncServer.publishAcquireMessage(completer);
 
       // Aguarda até que o recurso possa ser acessado e depois liberado
-      var accessGranted = await syncServer.listenForMessages(acquireTimestamp);
+      final bool result = await completer.future;
 
-      if (accessGranted) {
+      if (result) {
         // Retorna 200 OK após receber o RELEASE e remover da fila
-        return Response.ok('Resource accessed and released successfully by server $serverId');
+        return Response.ok('Resource accessed and released successfully by sync $clusterSyncId');
       } else {
         return Response.internalServerError(body: 'Failed to access resource');
       }
@@ -238,6 +216,21 @@ void main(List<String> args) async {
   });
 
   // Inicializa o servidor Shelf na porta 8080
-  var server = await io.serve(handler, InternetAddress.anyIPv4, 8080);
+  final HttpServer server = await io.serve(handler, InternetAddress.anyIPv4, 8080);
   logger.info('Sync server is running on http://${server.address.host}:${server.port}');
+
+  // Inicia a escuta de mensagens do Pub/Sub
+  syncServer.listenMessages();
+
+  // final Completer<bool> completer = Completer();
+  // await syncServer.publishAcquireMessage(completer);
+
+  // final Completer<bool> completer2 = Completer();
+  // await syncServer.publishAcquireMessage(completer2);
+
+  // final Completer<bool> completer3 = Completer();
+  // await syncServer.publishAcquireMessage(completer3);
+
+  // Inicia o processamento de mensagens
+  syncServer.processMessages();
 }
